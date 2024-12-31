34c88a78330b8dca062a6cfa8f6ae063860247a0
bertauchekurtis <70301946+bertauchekurtis@users.noreply.github.com>
2024-12-31 07:38:57 -0800

    Update fetch_AC.py

--- python/fetch_AC.py
+++ python/fetch_AC.py
@@ -80,11 +80,11 @@ stringData = today.strftime("%b-%d-%Y")
 element = driver.find_element(By.XPATH, "//li[@class='button left-tab user-specific-tab rivalsCanvasTab']/div/div")
 # button is hidden so use js to click
 driver.execute_script("$(arguments[0]).click();", element)
-sleep(5)
+sleep(10)
 
 # grab all the clickable airlines
 airlineTabs = driver.find_elements(By.XPATH, "//div[@id='rivalsTable']//div[@class='table-row clickable']")
-sleep(1)
+sleep(5)
 # setup df
 listOfPlaneTypes = df['Model'].tolist()
 listOfPlaneTypes.insert(0, "Total")
